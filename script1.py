d05a0e48c949cd94a422d6a670dba418085ebaeb
Pizarro Anthony <apizarrom@est.ups.edu.ec>
2020-07-02 15:17:04 -0500

    Agregado datos del proyecto

---
+++ script1.py
@@ -0,0 +1,10 @@
+#script de prueba para git
+#autor: Pizarro Anthony <apizarrom@est.ups.edu.ec>
+#fecha: 2/7/2020
+#version 1.0.0
+
+def main():
+    print("hola mundo")
+    
+if __name__ == "__main__":
+        main()
